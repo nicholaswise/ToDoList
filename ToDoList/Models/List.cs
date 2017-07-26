84fd0eceebb133e515a76f0fc324d9e96a7ed0f4
Nick Wise <nickcwise@gmail.com>
2017-07-26 10:31:41 -0400

    initial commit:

---
+++ ToDoList/Models/List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class List
+    {
+        [Key]
+        public int ListID { get; set; }
+        public string ListTitle { get; set; }
+        public DateTime ListCreateDate { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+    }
+}
